d4358f255659762b538fa9af0c84321fa6560850
resteve <resteve@zikzakmedia.com>
2015-12-01 11:09:05 +0100

    Tests

--- tests/test_import_csv.py
+++ tests/test_import_csv.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the import_csv module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ImportCSVTestCase(unittest.TestCase):
-    'Test Import CSV module'
+class ImportCsvTestCase(ModuleTestCase):
+    'Test Import Csv module'
+    module = 'import_csv'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'import_csv')
-
-    def test0005views(self):
-        'Test views'
-        test_view('import_csv')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        ImportCSVTestCase))
+        ImportCsvTestCase))
     return suite
